5227e4af1323eed2e17ab6f761d29d4730ad992b
GauteM <40168197+GauteM@users.noreply.github.com>
2020-02-11 15:30:46 +0100

    Update chairs_per_person.py

--- chairs_per_person.py
+++ chairs_per_person.py
@@ -1,6 +1,15 @@
-f = open("reservations.csv")
+f = open("reservations")
 
+rad = {}
 for reservation in f:
     name, number = reservation.split(",")
-    chairs_per_person = 50 / int(number)
+    try:
+        chairs_per_person = 50 / int(number)
+    except ValueError:
+        rad[number.strip('\n')] = name
+        pass
+    except ZeroDivisionError:
+        rad[number.strip('\n')] = name
+        pass 
     print("{} will get {} chairs per person".format(name, chairs_per_person))
+print(rad)
